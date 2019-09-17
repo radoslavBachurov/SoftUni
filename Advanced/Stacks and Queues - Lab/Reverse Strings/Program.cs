cc5661cd5ec6b5e1ee76d1b6ac2f8fbfe08771d6
radoslavBachurov <radoslavbachurov@gmail.com>
2019-09-17 14:01:53 +0300

    Reverse String Task Finished

---
+++ Advanced/Stacks and Queues - Lab/Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> newStack = new Stack<char>(Console.ReadLine().ToCharArray());
+            int count = newStack.Count;
+
+            for (int i = 1; i <= count; i++)
+            {
+                Console.Write($"{newStack.Pop()}");
+            }
+        }
+    }
+}

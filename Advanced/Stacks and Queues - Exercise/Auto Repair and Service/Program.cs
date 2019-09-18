187df1ce851190d0aeaf34f6de13ce617c0581f9
radoslavBachurov <radoslavbachurov@gmail.com>
2019-09-18 23:10:22 +0300

    Tasks auto repair,Fashion Boutique Finished

---
+++ Advanced/Stacks and Queues - Exercise/Auto Repair and Service/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Auto_Repair_and_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> listOfCarsWaiting = new Queue<string>(Regex.Split(Console.ReadLine(), @"\s+"));
+            Stack<string> listOfCarsServed = new Stack<string>();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "Service")
+                {
+                    if (listOfCarsWaiting.Count > 0)
+                    {
+                        listOfCarsServed.Push(listOfCarsWaiting.Peek());
+                        string servedCar = listOfCarsWaiting.Dequeue();
+                        Console.WriteLine($"Vehicle {servedCar} got served.");
+                    }
+                }
+                else if (command == "History")
+                {
+                    Console.WriteLine(string.Join(", ", listOfCarsServed));
+                }
+                else
+                {
+                    string[] commandArr = command.Split("-").ToArray();
+                    string carToCheck = commandArr[1];
+
+                    if (listOfCarsWaiting.Contains(carToCheck))
+                    {
+                        Console.WriteLine("Still waiting for service.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Served.");
+                    }
+                }
+            }
+
+            if (listOfCarsWaiting.Count > 0)
+            {
+                Console.WriteLine($"Vehicles for service: {string.Join(", ", listOfCarsWaiting)}");
+            }
+
+            Console.WriteLine($"Served vehicles: {string.Join(", ", listOfCarsServed)}");
+
+        }
+    }
+}
---
+++ Advanced/Stacks and Queues - Exercise/Fashion Boutique/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+            int rackInitialCapacity = int.Parse(Console.ReadLine());
+            int rackCapacity = rackInitialCapacity;
+            int rackCount = 1;
+
+            while (clothes.Count > 0)
+            {
+                if (rackCapacity - clothes.Peek() < 0)
+                {
+                    rackCount++;
+                    rackCapacity = rackInitialCapacity;
+                }
+
+                rackCapacity -= clothes.Pop();
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}

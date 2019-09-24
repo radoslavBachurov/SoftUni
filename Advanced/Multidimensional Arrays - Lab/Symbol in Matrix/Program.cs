a0a093471e618db70dc67dde5ede8525f064eca4
radoslavBachurov <radoslavbachurov@gmail.com>
2019-09-24 15:25:58 +0300

    task Primary Diagonal.csproj,Sum Matrix Columns.csproj,Sum Matrix Elements.csproj finished

---
+++ Advanced/Multidimensional Arrays - Lab/Symbol in Matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

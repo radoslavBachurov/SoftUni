7fc08d51fa522dee2571b1bffe7bc8feb07f7597
radoslavBachurov <radoslavbachurov@gmail.com>
2019-09-26 13:43:10 +0300

    Task Jagged Array Manipulator.csproj,Knight Game.csproj Finished

---
+++ Advanced/Multidimensional Arrays - Exercise/Jagged Array Manipulator/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRows = int.Parse(Console.ReadLine());
+
+            double[][] jaggedIntMatrix = new double[numberRows][];
+
+            for (int i = 0; i < numberRows; i++)
+            {
+                double[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+                jaggedIntMatrix[i] = input;
+
+                if (i > 0)
+                {
+                    TakingInput(jaggedIntMatrix, i);
+                }
+            }
+
+            ExecutingCommands(numberRows, jaggedIntMatrix);
+
+            Printing(jaggedIntMatrix);
+            return;
+        }
+
+        private static void Printing(double[][] jaggedIntMatrix)
+        {
+            foreach (var item in jaggedIntMatrix)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+
+        private static void ExecutingCommands(int numberRows, double[][] jaggedIntMatrix)
+        {
+            string command = string.Empty;
+            while ((command = Console.ReadLine()?.ToLower()) != "end")
+            {
+                string[] commandArr = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string operation = commandArr[0];
+                int row = int.Parse(commandArr[1]);
+                int collum = int.Parse(commandArr[2]);
+                int value = int.Parse(commandArr[3]);
+
+                bool indexValidation = row >= 0 &&
+                    row < numberRows &&
+                    collum >= 0 &&
+                    collum < jaggedIntMatrix[row].Length;
+
+                if (operation.ToLower() == "add" && indexValidation)
+                {
+                    jaggedIntMatrix[row][collum] += value;
+                }
+                else if (operation.ToLower() == "subtract" && indexValidation)
+                {
+                    jaggedIntMatrix[row][collum] -= value;
+                }
+            }
+        }
+
+        private static void TakingInput(double[][] jaggedIntMatrix, int i)
+        {
+            if (jaggedIntMatrix[i].Length == jaggedIntMatrix[i - 1].Length)
+            {
+                for (int t = 0; t < jaggedIntMatrix[i].Length; t++)
+                {
+                    jaggedIntMatrix[i][t] *= 2.0;
+                    jaggedIntMatrix[i - 1][t] *= 2.0;
+                }
+            }
+            else
+            {
+                for (int t = 0; t < jaggedIntMatrix[i].Length; t++)
+                {
+                    jaggedIntMatrix[i][t] /= 2.0;
+                }
+                for (int t = 0; t < jaggedIntMatrix[i - 1].Length; t++)
+                {
+                    jaggedIntMatrix[i - 1][t] /= 2.0;
+                }
+            }
+        }
+    }
+}
+
---
+++ Advanced/Multidimensional Arrays - Exercise/Knight Game/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Knight_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int boardSize = int.Parse(Console.ReadLine());
+            Knight[,] board = new Knight[boardSize, boardSize];
+
+            for (int i = 0; i < boardSize; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for (int t = 0; t < boardSize; t++)
+                {
+                    if (input[t] == '0')
+                    {
+                        board[i, t] = new Knight(-1);
+                    }
+                    else
+                    {
+                        board[i, t] = new Knight(0);
+                    }
+                }
+            }
+
+            int countRemoves = 0;
+            bool search = true;
+            while (search)
+            {
+
+                for (int i = 0; i < boardSize; i++)
+                {
+                    for (int t = 0; t < boardSize; t++)
+                    {
+                        if (board[i, t].canTakeHits != -1)
+                        {
+                            board[i, t].canTakeHits = 0;
+                        }
+                    }
+                }
+
+                search = false;
+                for (int i = 0; i < boardSize; i++)
+                {
+                    for (int t = 0; t < boardSize; t++)
+                    {
+                        if (i - 2 >= 0 && t - 1 >= 0 && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i - 2, t - 1].canTakeHits != -1)
+                            {
+                                board[i - 2, t - 1].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (i - 2 >= 0 && t + 1 < boardSize && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i - 2, t + 1].canTakeHits != -1)
+                            {
+                                board[i - 2, t + 1].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (i + 2 < boardSize && t - 1 >= 0 && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i + 2, t - 1].canTakeHits != -1)
+                            {
+                                board[i + 2, t - 1].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (i + 2 < boardSize && t + 1 < boardSize && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i + 2, t + 1].canTakeHits != -1)
+                            {
+                                board[i + 2, t + 1].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (t - 2 >= 0 && i - 1 >= 0 && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i - 1, t - 2].canTakeHits != -1)
+                            {
+                                board[i - 1, t - 2].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (t - 2 >= 0 && i + 1 < boardSize && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i + 1, t - 2].canTakeHits != -1)
+                            {
+                                board[i + 1, t - 2].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (t + 2 < boardSize && i - 1 >= 0 && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i - 1, t + 2].canTakeHits != -1)
+                            {
+                                board[i - 1, t + 2].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                        if (t + 2 < boardSize && i + 1 < boardSize && board[i, t].canTakeHits != -1)
+                        {
+                            if (board[i + 1, t + 2].canTakeHits != -1)
+                            {
+                                board[i + 1, t + 2].canTakeHits++;
+                                search = true;
+                            }
+                        }
+                    }
+                }
+
+                int max = int.MinValue;
+                int indexRow = -1;
+                int indexColl = -1;
+
+                for (int i = 0; i < boardSize; i++)
+                {
+                    for (int t = 0; t < boardSize; t++)
+                    {
+                        if (board[i, t].canTakeHits > max)
+                        {
+                            max = board[i, t].canTakeHits;
+                            indexRow = i;
+                            indexColl = t;
+                        }
+                    }
+                }
+
+                if (max != -1 && max != 0)
+                {
+                    board[indexRow, indexColl].canTakeHits = -1;
+                    countRemoves++;
+                }
+            }
+
+            Console.WriteLine(countRemoves);
+        }
+    }
+    class Knight
+    {
+        public Knight(int input)
+        {
+            canTakeHits = input;
+        }
+        public int canTakeHits { get; set; }
+    }
+}

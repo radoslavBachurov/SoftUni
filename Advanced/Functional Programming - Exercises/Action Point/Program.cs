614a2f38552ab75e86923bc27ebbf52e9b8d5b9c
radoslavBachurov <radoslavbachurov@gmail.com>
2019-10-10 19:13:17 +0300

    tasks Action Point,Knights of Honor finished

---
+++ Advanced/Functional Programming - Exercises/Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printing = names => Console.WriteLine(string.Join(Environment.NewLine,names));
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            printing(input);
+        }
+    }
+}
---
+++ Advanced/Functional Programming - Exercises/Custom Min Function/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Func<int,int> smallestNumber =  
+        }
+    }
+}

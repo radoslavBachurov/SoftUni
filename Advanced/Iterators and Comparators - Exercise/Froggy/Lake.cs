7ecb4a164086787cd494e3e944b1af622341815e
radoslavBachurov <radoslavbachurov@gmail.com>
2019-10-31 21:03:19 +0200

    froggy modified

--- Advanced/Iterators and Comparators - Exercise/Froggy/Lake.cs
+++ Advanced/Iterators and Comparators - Exercise/Froggy/Lake.cs
@@ -9,40 +9,29 @@ namespace Froggy
     public class Lake : IEnumerable<int>
     {
         List<int> sortedStones;
-        int[] input;
+
         public Lake(int[] input)
         {
-            this.input = input;
-            sortedStones = new List<int>();
-            this.Sorting();
-        }
-
-        private void Sorting()
-        {
-            var odd = new List<int>();
-            var even = new List<int>();
-            int counter = 0;
-            for (int i = 1; i <= this.input.Length; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    even.Add(input[counter]);
-                }
-                else
-                {
-                    odd.Add(input[counter]);
-                }
-                counter++;
-            }
-            even.Reverse();
-
-            sortedStones.AddRange(odd);
-            sortedStones.AddRange(even);
+            sortedStones = input.ToList();
         }
 
         public IEnumerator<int> GetEnumerator()
         {
-            return new LakeEnumerator(sortedStones);
+            for (int i = 0; i < sortedStones.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return sortedStones[i];
+                }
+            }
+
+            for (int i = sortedStones.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return sortedStones[i];
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()

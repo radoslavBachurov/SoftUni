49b9ab4602c759af0e93957d616b6fab21115479
radoslavbachurov <radoslavbachurov@gmail.com>
2019-11-06 23:06:59 +0200

    Task Person Finished

---
+++ OOP/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value > -1)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}

0bf58df488b703087d679645ebe04b1c72fd7d3b
radoslavBachurov <radoslavbachurov@gmail.com>
2019-11-22 10:31:29 +0200

    test

---
+++ OOP/Reflection and Attributes  Lab/Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, params string[] fieldsNames)
+        {
+            var classToHack = Type.GetType($"{typeof(StartUp).Namespace}.{name}");
+            FieldInfo[] fields = classToHack.GetFields(BindingFlags.NonPublic |
+                BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+
+            Object classinstance = Activator.CreateInstance(classToHack, new object[] { });
+
+            StringBuilder newSb = new StringBuilder();
+            newSb.AppendLine($"Class under investigation: {classToHack.Name}");
+
+            foreach (var field in fields.Where(x => fieldsNames.Contains(x.Name)))
+            {
+                newSb.AppendLine($"{field.Name} = {field.GetValue(classinstance)}");
+            }
+
+            return newSb.ToString().Trim();
+        }
+    }
+}

78309ac71ef74aaf77ad2e953c49620bcda88513
radoslavBachurov <radoslavbachurov@gmail.com>
2019-12-05 17:54:27 +0200

    mod

---
+++ OOP/Exams/C# OOP Exam - 14 April 2019/MortalEnginesSkeleton/MortalEngines/Machines/Fighter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Machines
+{
+    public class Fighter : BaseMachine, IFighter
+    {
+        public Fighter(string name, double attackPoints, double defensePoints)
+            : base(name, attackPoints, defensePoints, 200)
+        {
+            ToggleAggressiveMode();
+        }
+
+        public bool AggressiveMode { get; private set; }
+
+        public void ToggleAggressiveMode()
+        {
+            if (AggressiveMode)
+            {
+                this.AggressiveMode = false;
+                this.AttackPoints -= 50;
+                this.DefensePoints += 25;
+            }
+            else
+            {
+                this.AggressiveMode = true;
+                this.AttackPoints += 50;
+                this.DefensePoints -= 25;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder newSB = new StringBuilder();
+            newSB.AppendLine(base.ToString());
+
+            if (AggressiveMode)
+            {
+                newSB.AppendLine($" *Aggressive: ON");
+            }
+            else
+            {
+                newSB.AppendLine($" *Aggressive: OFF");
+            }
+
+            return newSB.ToString().Trim();
+        }
+    }
+}
---
+++ OOP/Exams/C# OOP Exam - 14 April 2019/MortalEnginesSkeleton/MortalEngines/Machines/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MortalEngines.Entities.Contracts;
+
+namespace MortalEngines.Machines
+{
+    public class Tank : BaseMachine, ITank
+    {
+        public Tank(string name, double attackPoints, double defensePoints)
+            : base(name, attackPoints, defensePoints, 100)
+        {
+            ToggleDefenseMode();
+        }
+
+        public bool DefenseMode { get; private set; }
+
+        public void ToggleDefenseMode()
+        {
+            if (DefenseMode)
+            {
+                this.DefenseMode = false;
+                this.AttackPoints += 40;
+                this.DefensePoints -= 30;
+            }
+            else
+            {
+                this.DefenseMode = true;
+                this.AttackPoints -= 40;
+                this.DefensePoints += 30;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder newSB = new StringBuilder();
+            newSB.AppendLine(base.ToString());
+
+            if (DefenseMode)
+            {
+                newSB.AppendLine($" *Defense: ON");
+            }
+            else
+            {
+                newSB.AppendLine($" *Defense: OFF");
+            }
+
+            return newSB.ToString().Trim();
+        }
+    }
+}

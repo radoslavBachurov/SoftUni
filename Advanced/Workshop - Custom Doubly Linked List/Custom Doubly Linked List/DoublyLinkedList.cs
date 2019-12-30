d764ba01a1873f1348ba79573786a38fda96cff0
radoslavBachurov <radoslavbachurov@gmail.com>
2019-12-30 23:15:53 +0200

    custom doubly linked list finished

---
+++ Advanced/Workshop - Custom Doubly Linked List/Custom Doubly Linked List/DoublyLinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        private class Node
+        {
+            public Node(int value)
+            {
+                this.value = value;
+            }
+
+            public int value { get; private set; }
+            public Node previousNode { get; set; }
+            public Node nextNode { get; set; }
+        }
+
+        private Node Head;
+        private Node Tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            Node nodeToAdd = new Node(element);
+
+            if (Count == 0)
+            {
+                this.Head = nodeToAdd;
+                this.Tail = nodeToAdd;
+            }
+            else
+            {
+                nodeToAdd.nextNode = this.Head;
+                this.Head.previousNode = nodeToAdd;
+                this.Head = nodeToAdd;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            Node nodeToAdd = new Node(element);
+
+            if (Count == 0)
+            {
+                this.Head = nodeToAdd;
+                this.Tail = nodeToAdd;
+            }
+            else
+            {
+                nodeToAdd.previousNode = this.Tail;
+                this.Tail.nextNode = nodeToAdd;
+                this.Tail = nodeToAdd;
+            }
+
+            this.Count++;
+        }
+
+        public void RemoveFirst()
+        {
+            CheckIfInvalidCount();
+            this.Head = this.Head.nextNode;
+            this.Head.previousNode = null;
+
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+            CheckIfInvalidCount();
+            this.Tail = this.Tail.previousNode;
+            this.Tail.nextNode = null;
+
+            Count--;
+        }
+
+        public void ForEach()
+        {
+            var currentNode = this.Head;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.value);
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            CheckIfInvalidCount();
+
+            var currentNode = this.Head;
+
+            int[] arrToReturn = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                arrToReturn[i] = currentNode.value;
+                currentNode = currentNode.nextNode;
+            }
+
+            return arrToReturn;
+        }
+
+        private void CheckIfInvalidCount()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("There is no elements in the linked list");
+            }
+        }
+
+
+    }
+}
---
+++ Advanced/Workshop - Custom Doubly Linked List/Custom Doubly Linked List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList MyList = new DoublyLinkedList();
+            // 2 1 4 5 
+            MyList.AddFirst(1);
+            MyList.AddFirst(2);
+            MyList.AddFirst(3);
+            MyList.AddLast(4);
+            MyList.AddLast(5);
+            MyList.AddLast(6);
+            MyList.RemoveFirst();
+            MyList.RemoveLast();
+
+            MyList.ForEach();
+            int[] output = MyList.ToArray();
+            Console.WriteLine(string.Join(" ",output));
+        }
+    }
+}
